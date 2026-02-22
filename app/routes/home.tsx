224ea291bd42147c0dbd2b5249851ba2e1eb9bd4
mhnmufees <mhn.mufees@gmail.com>
2026-02-22 13:26:09 +0530

    implementing Home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import NavBar from "components/NavBar";
 import type { Route } from "./+types/home";
+import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
+import { Button } from "components/ui/Buttion";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,8 +14,77 @@ export default function Home() {
   return (
     <div className="home">
       <NavBar />
-      <h1 className="text-3xl text-indigo-700 font-extrabold">Welcome</h1>
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+          <p>Introducing Roomify 2.0</p>
+        </div>
+        <h1>Build beautiful spaces at the speed of thought with Roomify</h1>
+        <p className="subtitle">
+          Roomify is an AI-first design environment that helps you visualize, render, and ship architectural projects faster than ever.
+        </p>
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start building <ArrowRight className="icon" />
+          </a>
+          <Button className="demo" size="lg" variant="outline">Watch Demo </Button>
+        </div>
+
+        <div className="upload-shell" id="upload">
+          <div className="grid-overlay" />
+
+            <div className="upload-card">
+              <div className="upload-head">
+                <div className="upload-icon">
+                  <Layers className="icon" />
+                </div>
+
+                <h3>Upload your floor plan!</h3>
+                <p>Supports PNG, JPG format up to 10MB</p>
+              </div>
+
+              <p>Upload Images</p>
+            </div>
+        </div>
+      </section>
       
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>Your latest work and shared community projects, all in one place.</p>
+            </div>
+          </div>
+
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img src="" alt="Project preview" />
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Project Manhattan</h3>
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date('01, 01, 2027').toLocaleDateString()}</span>
+                    <span>By JSM</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   ) 
 }
